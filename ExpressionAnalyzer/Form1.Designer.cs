c9bfbc6341b1f058848d56b613401f213e6ff363
rudinehartanto <rudinesurya@gmail.com>
2019-01-25 21:15:29 +0000

    transferred

---
+++ ExpressionAnalyzer/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ExpressionAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTxt = new System.Windows.Forms.TextBox();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTxt
+            // 
+            this.inputTxt.Location = new System.Drawing.Point(12, 141);
+            this.inputTxt.Name = "inputTxt";
+            this.inputTxt.Size = new System.Drawing.Size(394, 22);
+            this.inputTxt.TabIndex = 0;
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(331, 169);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 1;
+            this.okBtn.Text = "ok";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.Çalculate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 373);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.inputTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTxt;
+        private System.Windows.Forms.Button okBtn;
+    }
+}
+
---
+++ ExpressionAnalyzer/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExpressionAnalyzer
+{
+    public enum OP
+    {
+        AND,
+        OR
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Çalculate(object sender, EventArgs e)
+        {
+            string input = inputTxt.Text.ToUpper();
+            Stack<string> stack = new Stack<string>();
+
+            string[] splitted = input.Split(' ');
+
+            for (int i = 0; i < splitted.Length; ++i)
+            {
+                string value = splitted[i];
+
+                if (value.Equals(")"))
+                {
+                    List<string> data = new List<string>();
+
+                    while (true)
+                    {
+                        var value2 = stack.Pop();
+                        
+                        if (value2.Equals("("))
+                        {
+                            data.Reverse(); //because the data pushed in is in wrong order
+                            var r = Calc(data.ToArray());
+                            //push the result back into the stack
+                            stack.Push(r ? "T" : "F");
+                            break;
+                        }
+                        else
+                        {
+                            data.Add(value2);
+                        }
+                    }
+                }
+                else
+                    stack.Push(splitted[i]);
+            }
+            
+            //calculate the final statement. At this point there is no more brackets inside
+            bool result = Calc(stack.ToArray());
+
+            MessageBox.Show(result ? "T" : "F");
+        }
+        
+        /// <summary>
+        /// expressions passed inside will be evaluated from left to right, 
+        /// regardless of precedence
+        /// </summary>
+        /// <param name="exp"></param>
+        /// <returns></returns>
+        bool Calc(params string[] exp)
+        {
+            bool result = exp[0].Equals("T") ? true : false;
+            for (int i = 1; i < exp.Length; i+=2)
+            {
+                result = Calc(result, exp[i + 1].Equals("T") ? true : false, exp[i]);
+            }
+            return result;
+        }
+
+        bool Calc(bool a, bool b, string opStr)
+        {
+            OP op = opStr.Equals("&&") ? OP.AND : OP.OR;
+            
+            bool result = false;
+
+            switch (op)
+            {
+                case OP.AND:
+                    result = a && b;
+                    break;
+
+                case OP.OR:
+                    result = a || b;
+                    break;
+            }
+            return result;
+        }
+    }
+}
